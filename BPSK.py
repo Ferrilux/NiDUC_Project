283c549f6326e969c51f998431031f8e236337cb
Mikołaj Partykowski <miko.party@gmail.com>
2019-05-20 13:05:59 +0200

    Added description to code

--- BPSK.py
+++ BPSK.py
@@ -1,9 +1,11 @@
 import cv2
 import numpy as nm
 
-image = cv2.imread('example.jpg',0)
+image = cv2.imread('example.jpg',0) #wczytanie pliku jpg
 
-ret, bw_img = cv2.threshold(image,127,255,cv2.THRESH_BINARY)
-cv2.imshow("Binary Image",bw_img)
+ret, bw_img = cv2.threshold(image,127,255,cv2.THRESH_BINARY) #konwersja na tablice binara
+cv2.imshow("Binary Image",bw_img) #testowe wyswietlenie przekonwertowanego obrazu
 
+data = bw_img #zapis danych do nowej zmiennej
 
+print(len(data)) #wyswietlenie dlugosci tablicy (kontrolnie, mozna wywalic)
